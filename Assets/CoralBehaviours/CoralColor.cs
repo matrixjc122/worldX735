49cfb6f6a23f5df46f17602c7120bb3aa5a808ff
jcaonr <jcaonr@gmail.com>
2015-01-28 02:12:01 +0100

    Boden Textur / Pflanzen in Grau

---
+++ Assets/CoralBehaviours/CoralColor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoralColor : CoralBase {
+	
+	void Start () {
+
+				
+		string current_coral_type = worldXSingelton.Instance.m_CoralType;
+					
+								
+						
+
+						
+		if(current_coral_type == "TrichterC3_A0"){
+
+		}
+
+	}
+		
+
+	#region CoralInterface implementation
+
+	public override CoralBase CopyOf (CoralBase other)
+	{
+		return this;
+	}
+
+	#endregion
+}
