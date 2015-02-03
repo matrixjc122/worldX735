b7742469fff97a1e9eb393bd2bf51313de509afe
jcaonr <jcaonr@gmail.com>
2015-02-03 17:22:21 +0100

    Rahmen

--- Assets/UI/UIMain.cs
+++ Assets/UI/UIMain.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class UIMain : MonoBehaviour {
 
+	public Texture icon;
 	// Use this for initialization
 	void OnGUI()
 	{
 		// Make a group on the center of the screen
-		GUILayout.BeginArea (new Rect(30,Screen.height*0.5f-50,100,400));
+		GUILayout.BeginArea (new Rect(15,Screen.height*0.5f-20,100,400));
 
 		IEnumerable keyCollection = worldXSingelton.Instance.m_CoralPropertyDict.Keys;
 
@@ -17,6 +18,7 @@ public class UIMain : MonoBehaviour {
 			{
 				Debug.Log(coral_name + " selected!");
 				worldXSingelton.Instance.m_CoralType = coral_name;
+
 
 			}
 		}
