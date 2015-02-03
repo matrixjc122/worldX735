278ad7b6f62e1e0daa533e2a268db7b62a08d91a
jcaonr <jcaonr@gmail.com>
2015-02-03 09:48:40 +0100

    Steuerung/ Sprite Animation Test

--- Assets/Scripts/MoveCameraXZ.cs
+++ Assets/Scripts/MoveCameraXZ.cs
@@ -5,25 +5,37 @@ public class MoveCameraXZ : MonoBehaviour {
 
 	public float dragSpeed = 25;
 	void Start()
-	{
+	{	
+		camera.orthographicSize = 5;
 		transform.rotation = Quaternion.Euler(45, 315, 0);
 		transform.position = new Vector3 (7, 10, -7);
 	}
 	void Update()
 	{
-		if (Input.GetMouseButton(1))
-		{
+
+		// == MOVE/PAN XZ
+		if (Input.GetMouseButton(1)){
 			var h = -(Input.GetAxis("Mouse X"));
 			var v = -(Input.GetAxis("Mouse Y"));
 			Vector3 move = new Vector3(h, 0, v) * Time.deltaTime * dragSpeed;
 			transform.Translate (move);
 		}
+
+		// ==ZOOM/ORTHOGRAPHIC SIZE
+		if (Input.GetKeyDown ("down")) {
+			camera.orthographicSize += 0.25f;
+		}
+		
+		if (Input.GetKeyDown ("up")) {
+			camera.orthographicSize -= 0.25f;
+		}
+
 		transform.position = new Vector3 (transform.position.x,
 		                                  10,
 		                                  transform.position.z);
+
 	}
 }
-
 
 
 
