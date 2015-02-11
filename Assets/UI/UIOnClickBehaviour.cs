d99d628db63e4bc044ff08da331c607111d896a0
matrixjc122 <Jonas_C@gmx.de>
2015-02-11 01:05:35 +0100

    Add new behaviour for UI Canvas

---
+++ Assets/UI/UIOnClickBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIOnClickBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button b = gameObject.GetComponent<Button>();
+		b.onClick.AddListener(() => OnClick());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick()
+	{
+		if (gameObject.name == "ButtonA")
+			worldXSingelton.UISelectedType = "A";
+		if (gameObject.name == "ButtonB")
+			worldXSingelton.UISelectedType = "B";
+		if (gameObject.name == "ButtonC")
+			worldXSingelton.UISelectedType = "C";
+		if (gameObject.name == "ButtonBoden")
+			worldXSingelton.UISelectedType = "Boden";
+	}
+
+}
